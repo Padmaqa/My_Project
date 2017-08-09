2f16a86b706ed90959326a7d90f3a8fcef83683a
Priya <Priya@DESKTOP-1BS70QU>
2017-08-09 12:30:07 -0700

    This is my second commit for the demo code.

--- GitDemoProject/src/Git_Java_Demo.java
+++ GitDemoProject/src/Git_Java_Demo.java
@@ -3,6 +3,7 @@ public class Git_Java_Demo {
 
 	public static void main(String[] args) {
 System.out.println("This is my Firstjava Git Demo");
+System.out.println("This is my second commit  Git Demo");
 	}
 
 }
